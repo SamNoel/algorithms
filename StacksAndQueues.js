a5936667118ffed374dc73d8d296c8d4b8f86f50
SamNoel <32908864+SamNoel@users.noreply.github.com>
2025-01-29 16:41:14 -0500

    Stacks and Linter function

---
+++ StacksAndQueues.js
@@ -0,0 +1,92 @@
+/** A stack stores data in the same way that arrays do—it’s simply a list of
+elements. The one catch is that stacks have the following three constraints:
+
+-- Data can only be inserted at the end of a stack.
+-- Data can only be read from the end of a stack.
+-- Data can only be removed from the end of a stack.
+
+Stacks are LIFO - Last In First Out
+
+Stacks are ideal for processing any data that should be handled in reverse
+order to how it was received (LIFO). The “undo” function in a word
+processor, or function calls in a networked application, are examples of when
+you’d want to use a stack.
+
+-- PG 144-152
+ */
+
+var stack = []; // use a simple array for our stack
+var errors = []; // to hold our errors
+
+// create a hashmap to efficiently check the opening and closing pairs
+var braceKeyValuePairs = new Map();
+braceKeyValuePairs.set("}", "{");
+braceKeyValuePairs.set("]", "[");
+braceKeyValuePairs.set(")", "(");
+
+function codeLinter(text) {
+  var currentChar = null; // hold the current char of the text
+
+  for (var i = 0; i < text.length; i++) {
+    currentChar = text[i];
+
+    if (checkForOpeningBrace(currentChar)) {
+      // If the character is an opening brace, we push it onto the stack:
+      stack.push(currentChar);
+      //console.log("Pushed to stack: " + stack);
+    } else if (checkForClosingBrace(currentChar)) {
+      if (checkForClosingMostRecentBrace(currentChar)) {
+        /** If the character closes the most recent opening brace, we pop the opening brace from our stack: */
+        stack.pop();
+        //console.log("Popped from stack: " + stack);
+      } else {
+        // if the character does NOT close the most recent opening brace
+        errors.push(`Incorrect closing brace: ${currentChar} at index ${i}`);
+      }
+    }
+  }
+
+  if (stack.length > 0) {
+    /** If we get to the end of line, and the stack isn’t empty, that means
+     * we have an opening brace without a corresponding closing brace: */
+    errors.push(`${stack[stack.length - 1]} does not have a closing brace`);
+  }
+
+  if (errors.length > 0) {
+    console.log(errors);
+  } else {
+    console.log("No errors in text.");
+  }
+}
+
+function checkForOpeningBrace(char) {
+  return ["{", "[", "("].includes(char);
+}
+
+function checkForClosingBrace(char) {
+  return ["}", "]", ")"].includes(char);
+}
+
+function checkForClosingMostRecentBrace(char) {
+  var mostRecentOpeningBrace = getMostRecentOpeningBrace(stack);
+
+  // If no opening brace, return false
+  if (mostRecentOpeningBrace == undefined) {
+    return false;
+  }
+
+  // Otherwise, check closing brace against opening brace to see if its a pair
+  return braceKeyValuePairs.get(char) == mostRecentOpeningBrace;
+}
+
+function getMostRecentOpeningBrace(array) {
+  if (array.length > 0) {
+    return array[array.length - 1];
+  }
+
+  return undefined;
+}
+
+var textVal = "(var x = {y: [1, 2, 3]})";
+
+codeLinter(textVal);
